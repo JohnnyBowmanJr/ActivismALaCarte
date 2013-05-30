a0e08511f31893b3fed13aaeaed85e1576098122
Johnny Bowman <JohnnyBowman@gmail.com>
2013-05-30 07:06:32 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Activist::Application.config.secret_token = '5858b65d546baa437b48308ed89ab41fdca4c9249c66ed799886319a158dc22369a47a2f93d0ec79f540206706eaf5ca1da260df6cd15bf40c06ec04ed70e322'
